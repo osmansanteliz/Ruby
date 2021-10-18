2430e4692d5f4503363bbdcead36e07ec4a7645f
Osman Santeliz <osanteliz88@gmail.com>
2021-10-18 11:30:05 -0600

    osanteliz/operadores de comparacion  * En esta seccion aprendimo sobre los operadores de comparacion y  * practicamos un poco con el condicional if.

---
+++ operadorescomparacion.rb
@@ -0,0 +1,29 @@
+a = 2
+b = 3
+c = 2
+
+if (a == b)
+  puts ("tienen el mismo valor")
+else
+  puts "no tienen el mismo valor"
+end
+
+if (a != c)
+  puts ("no tienen el mismo valor")
+else
+  puts "tienen el mismo valor"
+end
+
+if a < c
+  puts "a es menor que c"
+else
+  puts "a no es menor que c"
+end
+
+if b > c
+  puts "b es mayor"
+else
+  puts "b es menor"
+end
+
+#tambien tenemos los <= , >=
